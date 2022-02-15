d92d93d3fcedae7abc85a616ed3ba0c0ae798fe5
srezasm <srezas.2017@gmail.com>
2022-02-15 12:49:50 +0330

    qr-code cards improved

--- index.js
+++ index.js
@@ -11,11 +11,31 @@ function handleChange(id) {
     })
         .then((response) => {
             response.json().then((res) => {
+                // var qrDiv = document.getElementById("qr-div");
+                // var qrId = `${id}-qr`;
+                // qrDiv.innerHTML =
+                //     qrDiv.innerHTML +
+                //     `<div class="mt-4"> <small>${res.name}</small><div class="mt-1 mb-1" id="${qrId}"></div> <small>${res.link}</small> </div>`;
+
+                // var qrcode = new QRCode(
+                //     document.getElementById(qrId),
+                //     res.link
+                // );
+                // qrcode.makecode();
+
                 var qrDiv = document.getElementById("qr-div");
                 var qrId = `${id}-qr`;
                 qrDiv.innerHTML =
                     qrDiv.innerHTML +
-                    `<div class="mt-4"> <small>${res.name}</small><div class="mt-1 mb-1" id="${qrId}"></div> <small>${res.link}</small> </div>`;
+                    `<div class="card col-lg-3 col-md-6 col-12 m-1">` +
+                    `<div id="${qrId}" class="card-img-top mt-2"></div>` +
+                    `<div class="card-body">`+
+                    `<h5 class="card-title">${res.name}</h5>` +
+                    `<a class="card-text" href="${res.link}">${res.link}</a>` +
+                    `<a class="btn btn-primary" onclick="navigator.clipboard.writeText('${res.link}')">Copy link</a>` +
+                    `</div>` +
+                    `</div>`;
+
 
                 var qrcode = new QRCode(
                     document.getElementById(qrId),
