58fbe6c891150102d09ecdbd338ff3270d61d672
srezasm <srezas.2017@gmail.com>
2022-02-15 08:17:54 +0330

    everything works fine!

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-//todo: How to import .js file in javascript
 const formElement = document.getElementById("form");
 
 function handleChange(id) {
-    //! to test the qr code
-    // var qrDiv = document.getElementById("qr-div");
-    // var qrId = `${id}-qr`;
-    // qrDiv.innerHTML =
-    //     qrDiv.innerHTML +
-    //     `<div class="col-2"> <div id="${qrId}"></div> <br/> <span>https://testlink.com/hhahha/10239</span> </div>`;
-    // var qrcode = new QRCode(document.getElementById(qrId), "https://testlink.com/hhahha/10239");
-    // qrcode.makecode();
-
-    // handle add new file inputs
-    var newId = id + 1;
-    var element = `<input id="${newId}" class="m-2" type="file" onchange="handleChange(${newId})">`;
-    formElement.innerHTML = formElement.innerHTML + element;
-
     var data = new FormData();
     data.append("file", document.getElementById(id).files[0]);
-    console.log(document.getElementById(id).files[0]);
     document.getElementById(id).setAttribute("disabled", "true");
 
     fetch("https://file.io/", {
@@ -31,16 +15,18 @@ function handleChange(id) {
                 var qrId = `${id}-qr`;
                 qrDiv.innerHTML =
                     qrDiv.innerHTML +
-                    `<div class="mt-4"> <small>${res.name}name </small><div class="mt-1 mb-1" id="${qrId}"></div> <small>${res.link}https://testlink.com/hhahha/10239</small> </div>`;
+                    `<div class="mt-4"> <small>${res.name}</small><div class="mt-1 mb-1" id="${qrId}"></div> <small>${res.link}</small> </div>`;
 
-                var qrcode = new QRCode(document.getElementById(qrId), "res.linkhttps://testlink.com/hhahha/10239");
+                var qrcode = new QRCode(document.getElementById(qrId), res.link);
                 qrcode.makecode();
             });
-        })
-        .then((result) => {
-            console.log("Success:", result);
         })
         .catch((error) => {
             console.error("Error:", error);
         });
+
+    // handle add new file inputs
+    var newId = id + 1;
+    var element = `<input id="${newId}" class="m-2" type="file" onchange="handleChange(${newId})">`;
+    formElement.innerHTML = formElement.innerHTML + element;
 }
